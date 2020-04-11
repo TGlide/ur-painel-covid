2d0ca08a8f6e966f71ca29b4b32d6a70c520ba99
tglide <thomglopes@gmail.com>
2020-04-11 13:23:11 -0300

    :sparkles: add granular AP

--- scrapper/get_AP.py
+++ scrapper/get_AP.py
@@ -8,20 +8,30 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 from unidecode import unidecode
 
-file_path = os.path.join(os.getcwd(), "scrapper", "bairro_ap_v2.xlsx")
-write_path = os.path.join(os.getcwd(), "scrapper", "ap.json")
-df = pd.read_excel(file_path)
+file_path = os.path.join(os.getcwd(), "scrapper",
+                         "files", "in", "bairro_ap_novo.xlsx")
+write_path = os.path.join(os.getcwd(), "scrapper", "files", "out", "ap.json")
+df = pd.read_excel(file_path, "APs - granular")
 
 
 def remove_end_numbers(xs):
     return re.sub(r'(.*?)\s*\d+', r'\1', xs).strip()
 
 
-df['Bairros'] = df['Unnamed: 0'].apply(
-    remove_end_numbers).apply(lambda x: unidecode(x))[1:]
-df['AP'] = df['Unnamed: 1'][1:]
+print(df)
 
-data = {df['Bairros'][i].lower(): df['AP'][i] for i in range(1, len(df['AP']))}
+df['Bairros'] = df['Unnamed: 1'][2:]
+df['AP'] = df['Unnamed: 2'][2:]
+
+for i in range(6):
+    df.drop(f'Unnamed: {i}', axis=1, inplace=True)
+
+df.drop(df.index[0], inplace=True)
+df.drop(df.index[0], inplace=True)
+
+data = {unidecode(df['Bairros'][i].lower().strip()): df['AP'][i]
+        for i in range(2, len(df['AP']))}
+print(data)
 
 with open(write_path, 'w') as f:
     json.dump(data, f)
