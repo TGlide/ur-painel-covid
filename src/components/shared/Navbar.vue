254ba1e805ae96d6594ae05460a9c3d77c964938
tglide <thomglopes@gmail.com>
2020-04-19 13:13:00 -0300

    :sparkles: create projections view; add links to navbar

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <b-navbar type="is-dark">
     <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <b-navbar-item tag="div" :to="{ path: '/' }">
         <img
           src="@/assets/logo_dark.png"
           alt="Movimento União Rio"
@@ -10,6 +10,15 @@
         <h4 class="title is-4">
           Monitoramento União Rio
         </h4>
+      </b-navbar-item>
+    </template>
+
+    <template slot="start">
+      <b-navbar-item tag="router-link" :to="{ name: 'Home' }">
+        Início
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ name: 'Projections' }">
+        Projeções
       </b-navbar-item>
     </template>
   </b-navbar>
